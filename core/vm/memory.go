55ae1cb0880138838cc56815c331e3a7d6781e57
iAmGroute <iAmGroute@outlook.com>
2022-05-29 11:21:41 +0300

    core/vm/memory: Faster Set32()

--- core/vm/memory.go
+++ core/vm/memory.go
@@ -18,6 +18,7 @@ package vm
 
 import (
 	"fmt"
+	"encoding/binary"
 
 	"github.com/holiman/uint256"
 )
@@ -55,10 +56,12 @@ func (m *Memory) Set32(offset uint64, val *uint256.Int) {
 	if offset+32 > uint64(len(m.store)) {
 		panic("invalid memory: store empty")
 	}
-	// Zero the memory area
-	copy(m.store[offset:offset+32], []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
-	// Fill in relevant bits
-	val.WriteToSlice(m.store[offset:])
+	// Set each 64-bit-word individually, simillar to how val.Bytes32() does it, it's faster than val.WriteToSlice()
+	t := m.store[offset : offset + 32]
+	binary.BigEndian.PutUint64(t[ 0: 8], val[3])
+	binary.BigEndian.PutUint64(t[ 8:16], val[2])
+	binary.BigEndian.PutUint64(t[16:24], val[1])
+	binary.BigEndian.PutUint64(t[24:32], val[0])
 }
 
 // Resize resizes the memory to size
